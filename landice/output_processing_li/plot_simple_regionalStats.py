e2000c915832be20c0301611e8417e2206396def
Trevor Hillebrand <trhille@lanl.gov>
2023-12-01 08:42:30 -0800

    Add ability to plot global or regional stats
    Add ability to plot global or regional stats by specifying --regional
    Also add default colors in dictionaries for each ISMIP6-type run.
    Default colors are not yet actually used in plotting.

--- landice/output_processing_li/plot_simple_regionalStats.py
+++ landice/output_processing_li/plot_simple_regionalStats.py
@@ -32,6 +32,7 @@ parser.add_option("-u", dest="units", help="units for mass/volume: m3, kg, Gt", 
 parser.add_option("-n", dest="fileRegionNames", help="region name filename.  If not specified, will attempt to read region names from file 1.", metavar="FILENAME")
 parser.add_option("-r", dest="plot_regions", help="indices of regions to plot. comma-separated", default=None)  # Ross, ASE, FRIS = 7,9,14
 parser.add_option("-v", dest="plot_var", help="name of variable to plot", default="regionalVolumeAboveFloatation")
+parser.add_option("--regional", dest="regional", help="Whether to plot regional stats", action='store_true')
 options, args = parser.parse_args()
 
 print("Using ice density of {} kg/m3 if required for unit conversions".format(rhoi))
@@ -48,25 +49,7 @@ print("Using volume/mass units of: ", massUnit)
 
 # Get nRegions and yr from first file
 f = Dataset(options.file1inName, 'r')
-nRegions = len(f.dimensions['nRegions'])
 yr = f.variables['daysSinceStart'][:]/365.0
-if options.plot_regions is None:
-    plot_regions = range(nRegions)
-else:
-    plot_regions = [int(i) for i in options.plot_regions.split(',')]
-
-# Get region names from file
-if options.fileRegionNames:
-   fn = Dataset(options.fileRegionNames, 'r')
-   rNamesIn = fn.variables['regionNames'][:]
-else:
-   rNamesIn = f.variables['regionNames'][:]
-# Process region names
-rNamesOrig = list()
-for r in range(nRegions):
-    thisString = rNamesIn[r, :].tobytes().decode('utf-8').strip()  # convert from char array to string
-    rNamesOrig.append(''.join(filter(str.isalnum, thisString)))  # this bit removes non-alphanumeric chars
-
 # Antarctic data from:
 # Rignot, E., Bamber, J., van den Broeke, M. et al. Recent Antarctic ice mass loss from radar interferometry
 # and regional climate modelling. Nature Geosci 1, 106-110 (2008). https://doi.org/10.1038/ngeo102
@@ -76,34 +59,79 @@ for r in range(nRegions):
 # Rignot, E., S. Jacobs, J. Mouginot, and B. Scheuchl. 2013. Ice-Shelf Melting Around Antarctica. Science 341 (6143): 266-70. https://doi.org/10.1126/science.1235798.
 # Note: May want to switch to input+, net+
 # Note: Some ISMIP6 basins combine multiple Rignot basins.  May want to separate if we update our regions.
+# Also assign each region a color for plotting. These were chosen such that the three major WAIS basins would
+# look good when plotted together. The rest are fairly random, with an attempt to avoid colors that are too similar.
 ISMIP6basinInfo = {
-        'ISMIP6BasinAAp': {'name': 'Dronning Maud Land', 'input': [60,9], 'outflow': [60,7], 'net': [0, 11], 'shelfMelt': [57.5]},
-        'ISMIP6BasinApB': {'name': 'Enderby Land', 'input': [39,5], 'outflow': [40,2], 'net': [-1,5], 'shelfMelt': [24.6]},
-        'ISMIP6BasinBC': {'name': 'Amery-Lambert', 'input': [73, 10], 'outflow': [77,4], 'net': [-4, 11], 'shelfMelt': [35.5]},
-        'ISMIP6BasinCCp': {'name': 'Phillipi, Denman', 'input': [81, 13], 'outflow': [87,7], 'net':[-7,15], 'shelfMelt': [107.9]},
-        'ISMIP6BasinCpD': {'name': 'Totten', 'input': [198,37], 'outflow': [207,13], 'net': [-8,39], 'shelfMelt': [102.3]},
-        'ISMIP6BasinDDp': {'name': 'Mertz', 'input': [93,14], 'outflow': [94,6], 'net': [-2,16], 'shelfMelt': [22.8]},
-        'ISMIP6BasinDpE': {'name': 'Victoria Land', 'input': [20,1], 'outflow': [22,3], 'net': [-2,4], 'shelfMelt': [22.9]},
-        'ISMIP6BasinEF': {'name': 'Ross', 'input': [61+110,(10**2+7**2)**0.5], 'outflow': [49+80,(4**2+2^2)**0.5], 'net': [11+31,(11*2+7**2)**0.5], 'shelfMelt': [70.3]},
-        'ISMIP6BasinFG': {'name': 'Getz', 'input': [108,28], 'outflow': [128,18], 'net': [-19,33], 'shelfMelt': [152.9]},
-        'ISMIP6BasinGH': {'name': 'Thwaites/PIG', 'input': [177,25], 'outflow': [237,4], 'net': [-61,26], 'shelfMelt': [290.9]},
-        'ISMIP6BasinHHp': {'name': 'Bellingshausen', 'input': [51,16], 'outflow': [86,10], 'net': [-35,19], 'shelfMelt': [76.3]},
-        'ISMIP6BasinHpI': {'name': 'George VI', 'input': [71,21], 'outflow': [78,7], 'net': [-7,23], 'shelfMelt': [152.3]},
-        'ISMIP6BasinIIpp': {'name': 'Larsen A-C', 'input': [15,5], 'outflow': [20,3], 'net': [-5,6], 'shelfMelt': [32.9]},
-        'ISMIP6BasinIppJ': {'name': 'Larsen E', 'input': [8,4], 'outflow': [9,2], 'net': [-1,4], 'shelfMelt': [4.3]},
-        'ISMIP6BasinJK': {'name': 'FRIS', 'input': [93+142, (8**2+11**2)**0.5], 'outflow': [75+145,(4**2+7**2)**0.5], 'net': [18-4,(9**2+13**2)**0.5], 'shelfMelt': [155.4]},
-        'ISMIP6BasinKA': {'name': 'Brunt-Stancomb', 'input': [42+26,(8**2+7**2)**0.5], 'outflow': [45+28,(4**2+2**2)**0.5], 'net':[-3-1,(9**2+8**2)**0.5], 'shelfMelt': [10.4]}
+        'ISMIP6BasinAAp': {'name': 'Dronning Maud Land', 'color': 'tab:blue', 'input': [60,9], 'outflow': [60,7], 'net': [0, 11], 'shelfMelt': [57.5]},
+        'ISMIP6BasinApB': {'name': 'Enderby Land', 'color': 'tab:orange', 'input': [39,5], 'outflow': [40,2], 'net': [-1,5], 'shelfMelt': [24.6]},
+        'ISMIP6BasinBC': {'name': 'Amery-Lambert', 'color': 'tab:red', 'input': [73, 10], 'outflow': [77,4], 'net': [-4, 11], 'shelfMelt': [35.5]},
+        'ISMIP6BasinCCp': {'name': 'Phillipi, Denman', 'color': 'tab:brown', 'input': [81, 13], 'outflow': [87,7], 'net':[-7,15], 'shelfMelt': [107.9]},
+        'ISMIP6BasinCpD': {'name': 'Totten', 'color': 'tab:pink', 'input': [198,37], 'outflow': [207,13], 'net': [-8,39], 'shelfMelt': [102.3]},
+        'ISMIP6BasinDDp': {'name': 'Mertz', 'color': 'tab:gray', 'input': [93,14], 'outflow': [94,6], 'net': [-2,16], 'shelfMelt': [22.8]},
+        'ISMIP6BasinDpE': {'name': 'Victoria Land', 'color': 'tab:olive', 'input': [20,1], 'outflow': [22,3], 'net': [-2,4], 'shelfMelt': [22.9]},
+        'ISMIP6BasinEF': {'name': 'Ross', 'color': 'tab:green', 'input': [61+110,(10**2+7**2)**0.5], 'outflow': [49+80,(4**2+2^2)**0.5], 'net': [11+31,(11*2+7**2)**0.5], 'shelfMelt': [70.3]},
+        'ISMIP6BasinFG': {'name': 'Getz', 'color': 'lightcoral', 'input': [108,28], 'outflow': [128,18], 'net': [-19,33], 'shelfMelt': [152.9]},
+        'ISMIP6BasinGH': {'name': 'Thwaites/PIG', 'color': 'tab:purple', 'input': [177,25], 'outflow': [237,4], 'net': [-61,26], 'shelfMelt': [290.9]},
+        'ISMIP6BasinHHp': {'name': 'Bellingshausen', 'color': 'teal', 'input': [51,16], 'outflow': [86,10], 'net': [-35,19], 'shelfMelt': [76.3]},
+        'ISMIP6BasinHpI': {'name': 'George VI', 'color': 'maroon', 'input': [71,21], 'outflow': [78,7], 'net': [-7,23], 'shelfMelt': [152.3]},
+        'ISMIP6BasinIIpp': {'name': 'Larsen A-C', 'color': 'violet', 'input': [15,5], 'outflow': [20,3], 'net': [-5,6], 'shelfMelt': [32.9]},
+        'ISMIP6BasinIppJ': {'name': 'Larsen E', 'color': 'blueviolet', 'input': [8,4], 'outflow': [9,2], 'net': [-1,4], 'shelfMelt': [4.3]},
+        'ISMIP6BasinJK': {'name': 'FRIS', 'color': 'tab:cyan', 'input': [93+142, (8**2+11**2)**0.5], 'outflow': [75+145,(4**2+7**2)**0.5], 'net': [18-4,(9**2+13**2)**0.5], 'shelfMelt': [155.4]},
+        'ISMIP6BasinKA': {'name': 'Brunt-Stancomb', 'color': 'goldenrod', 'input': [42+26,(8**2+7**2)**0.5], 'outflow': [45+28,(4**2+2**2)**0.5], 'net':[-3-1,(9**2+8**2)**0.5], 'shelfMelt': [10.4]}
         }
 
-# Parse region names to more usable names, if available
-rNames = [None]*nRegions
-for r in range(nRegions):
-    if rNamesOrig[r] in ISMIP6basinInfo:
-        rNames[r] = ISMIP6basinInfo[rNamesOrig[r]]['name']
-    else:
-        rNames[r] = rNamesOrig[r]
+# Hard-code plot settings for ISMIP6 runs. These will only apply if plotting global stats,
+# as regional stats will need to use different colors for different regions.
+# Color corresponds to climate model, linestyle to scenario (solid for 8.5, dashed for 2.6),
+# and alpha (opacity) corresponds to Forcing (opaque for 2300, semi-transparent for Repeat).
+# Hydrofracture runs will all have their own panel, so no need for special plot settings for those.
+global_plot_settings = {
+        'hist': {'color': 'black', 'linestyle': 'solid', 'alpha': 1.},
+        'ctrlAE': {'color': 'black', 'linestyle': 'solid', 'alpha': 1.},
+        'expAE01': {'color': 'tab:gray', 'linestyle': 'dashed', 'alpha': 0.6},
+        'expAE02': {'color': 'tab:blue', 'linestyle': 'solid', 'alpha': 1.},
+        'expAE03': {'color': 'tab:orange', 'linestyle': 'solid', 'alpha': 1.},
+        'expAE04': {'color': 'tab:purple', 'linestyle': 'solid', 'alpha': 1.},
+        'expAE05': {'color': 'tab:pink', 'linestyle': 'solid', 'alpha': 1.},
+        'expAE06': {'color': 'tab:pink', 'linestyle': 'dashed', 'alpha': 1.},
+        'expAE07': {'color': 'tab:gray', 'linestyle': 'solid', 'alpha': 0.6},
+        'expAE08': {'color': 'tab:orange', 'linestyle': 'solid', 'alpha': 0.6},
+        'expAE09': {'color': 'tab:purple', 'linestyle': 'solid', 'alpha': 0.6},
+        'expAE10': {'color': 'tab:pink', 'linestyle': 'dashed', 'alpha': 1},
+        'expAE11': {'color': 'tab:blue', 'linestyle': 'solid', 'alpha': 1.},
+        'expAE12': {'color': 'tab:orange', 'linestyle': 'solid', 'alpha': 1.},
+        'expAE13': {'color': 'tab:purple', 'linestyle': 'solid', 'alpha': 1.},
+        'expAE14': {'color': 'tab:pink', 'linestyle': 'solid', 'alpha': 1.}
+        }
 
-#print(rNames)
+if options.regional:
+    nRegions = len(f.dimensions['nRegions'])
+    if options.plot_regions is None:
+        plot_regions = range(nRegions)
+    else:
+        plot_regions = [int(i) for i in options.plot_regions.split(',')]
+
+    # Get region names from file
+    if options.fileRegionNames:
+       fn = Dataset(options.fileRegionNames, 'r')
+       rNamesIn = fn.variables['regionNames'][:]
+    else:
+       rNamesIn = f.variables['regionNames'][:]
+    # Process region names
+    rNamesOrig = list()
+    for r in range(nRegions):
+        thisString = rNamesIn[r, :].tobytes().decode('utf-8').strip()  # convert from char array to string
+        rNamesOrig.append(''.join(filter(str.isalnum, thisString)))  # this bit removes non-alphanumeric chars
+
+    # Parse region names to more usable names, if available
+    rNames = [None]*nRegions
+    region_colors = [None]*nRegions
+    for r in range(nRegions):
+        if rNamesOrig[r] in ISMIP6basinInfo:
+            rNames[r] = ISMIP6basinInfo[rNamesOrig[r]]['name']
+            region_colors[r] = ISMIP6basinInfo[rNamesOrig[r]]['color']
+        else:
+            rNames[r] = rNamesOrig[r]
 
 ncol = 2
 nrow = 1
@@ -148,21 +176,19 @@ def plotStat(fname, addToLegend=False):
 
     name = fname
 
+    # Hard-code some settings for ISMIP6 sensitivity tests.
     if 'AE02' in fname:
         axs = [axs1[0]]  # for list comprehension plotting, below
     elif 'AE03' in fname:
         axs = [axs1[1]]  # for list comprehension plotting, below
-    elif 'hist' in fname:
+    elif 'hist' in fname or 'ctrlAE' in fname:
         axs = axs1
-
     if 'gamma21000' in fname or 'm10/' in fname or 'no_thermal' in fname:
        linestyle = 'dashed'
     elif 'gamma9620' in fname or 'm1/' in fname:
        linestyle = 'dotted'
     else:
        linestyle = 'solid'
-
-    colors = ['tab:green', 'tab:purple', 'tab:cyan']
 
     f = Dataset(fname,'r')
     yr = f.variables['daysSinceStart'][:]/365.0
@@ -180,19 +206,21 @@ def plotStat(fname, addToLegend=False):
         dt = np.concatenate((hist_dt, dt))
         plot_var = np.concatenate((hist_plot_var, plot_var))
 
-    #yr = yr-yr[0]  # uncomment to align all start dates
-    dtnR = np.tile(dt.reshape(len(dt),1), (1,nRegions))  # repeated per region with dim of nt,nRegions
-    nRegionsLocal = len(f.dimensions['nRegions'])
-    if nRegionsLocal != nRegions:
-        sys.exit(f"ERROR: Number of regions in file {fname} does not match number of regions in first input file!")
-
     # Fig 1: summary plot
     if "Volume" in options.plot_var:
         plot_var *= volUnitFactor
 
-    plot_var = plot_var[:,:] - plot_var[0,:]
-    for r, color in zip(plot_regions, colors):
-        [ax.plot(yr, plot_var[:,r], linestyle=linestyle, color=color, linewidth=2) for ax in axs]
+    plot_var = plot_var[:] - plot_var[0]
+    if options.regional:
+        colors = ['tab:green', 'tab:purple', 'tab:cyan']
+        dtnR = np.tile(dt.reshape(len(dt),1), (1,nRegions))  # repeated per region with dim of nt,nRegions
+        nRegionsLocal = len(f.dimensions['nRegions'])
+        if nRegionsLocal != nRegions:
+            sys.exit(f"ERROR: Number of regions in file {fname} does not match number of regions in first input file!")
+        for r, color in zip(plot_regions, colors):
+            [ax.plot(yr, plot_var[:,r], linestyle=linestyle, color=color, linewidth=2) for ax in axs]
+    else:
+        [ax.plot(yr, plot_var, linestyle=linestyle, linewidth=2) for ax in axs]
 
     f.close()
 
