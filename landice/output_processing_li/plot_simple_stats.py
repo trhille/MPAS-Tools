2a91b554942a1d41ce3606575aff98064d5cf49c
Trevor Hillebrand <trhille@lanl.gov>
2024-04-29 19:11:37 -0700

    Default to volumeAboveFloatation

--- landice/output_processing_li/plot_simple_stats.py
+++ landice/output_processing_li/plot_simple_stats.py
@@ -26,7 +26,7 @@ parser.add_argument("-r", dest="plot_regions", type=int, nargs='*',
                     help=("indices of regions to plot. Space delimited.",
                           "If not specified, will plot all available regions."),
                     default=None)  # Ross, ASE, FRIS = 7,9,14
-parser.add_argument("-v", dest="plot_var", help="name of variable to plot", default="regionalVolumeAboveFloatation")
+parser.add_argument("-v", dest="plot_var", help="name of variable to plot", default="volumeAboveFloatation")
 parser.add_argument('-s', dest='save_filename', default=None, help='Path to save .png to, if desired.')
 parser.add_argument('--start_year', dest='start_year', default=0., type=float, help='Year value to assign at beginning of time series.')
 parser.add_argument("--regional", dest="regional", help="Whether to plot regional stats", action='store_true')
